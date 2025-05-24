de767291423f0e73ea40b98db90e46002a3b063d
Md Hridoy Sheikh <138392668+hd31520@users.noreply.github.com>
2025-05-24 15:17:59 +0600

    Add files via upload

--- src/Context/FetchProvider.jsx
+++ src/Context/FetchProvider.jsx
@@ -6,7 +6,7 @@ const FetchProvider = ({ children }) => {
   const [featuredGroups, setFeaturedGroups] = useState([]);
 
   useEffect(() => {
-    fetch('  http://localhost:3000/groups')
+    fetch('  https://hobbyt-hub-server.vercel.app/groups')
       .then(res => res.json())
       .then(data => {
         setFeaturedGroups(data);
